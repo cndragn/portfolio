d56f0edcff7d1e49a050b6f89c56bbce35eaedfc
CNDavidson <cndragn@gmail.com>
2017-07-26 13:26:13 -0400

    Update stat tracker

--- js/stats.js
+++ js/stats.js
@@ -1,5 +1,5 @@
 <!-- Start of StatCounter Code for Default Guide -->
-
+<script type="text/javascript">
 var sc_project=3844398; 
 var sc_invisible=1; 
 var sc_security="34f6984a"; 
@@ -9,24 +9,10 @@ var scJsHost = (("https:" == document.location.protocol) ?
 document.write("<sc"+"ript type='text/javascript' src='" +
 scJsHost+
 "statcounter.com/counter/counter.js'></"+"script>");
-
-
+</script>
 <noscript><div class="statcounter"><a title="web analytics"
-href="http://statcounter.com/" target="_blank"><img
-class="statcounter"
-src="http://c.statcounter.com/3844398/0/34f6984a/1/"
-alt="web analytics"></a></div></noscript>
+href="http://statcounter.com/" target="_blank"><img class="statcounter"src="//c.statcounter.com/3844398/0/34f6984a/1/" alt="web analytics"></a></div></noscript>
 <!-- End of StatCounter Code for Default Guide -->
-
-<!-- Google Analytics -->
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-5723962-2', 'candicedavidson.com');
-  ga('send', 'pageview');
 
 
 <!-- Mouseflow -->
